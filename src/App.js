37ded413e66bdfe05610ee7ef2f90b9e843faad1
eithiri <eithiritun1997@gmail.com>
2022-02-01 16:27:48 +0630

    feat: Redux

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@
 // }
 
 // export default App;
+
 // import React from 'react';
 // class App extends React.Component {
 //  render() {
@@ -59,28 +60,89 @@
 // }
 // export default App;
 
-import React from 'react';
-class Item extends React.Component {
- render() {
- return (
- <li>
- {this.props.name},
- ${this.props.price}
- </li>
- );
- }
+// import React from 'react';
+// class Item extends React.Component {
+//  render() {
+//  return (
+//  <li>
+//  {this.props.name},
+//  ${this.props.price}
+//  </li>
+//  );
+//  }
+// }
+// class App extends React.Component {
+//  render() {
+//  return (
+//  <div>
+//  <h1>Hello React</h1>
+//  <ul>
+//  <Item name="Apple" price="0.99" />
+//  <Item name="Orange" price="0.89" />
+//  </ul>
+//  </div>
+//  )
+//  }
+// }
+// export default App;
+
+import React, { createRef } from 'react';
+import { connect } from 'react-redux';
+const Item = ({ name, price }) => (
+    <li>{name}, ${price}</li>
+)
+const App = props => {
+    let nameRef = createRef();
+    let priceRef = createRef();
+
+    const add = () => {
+        props.add(
+            props.items.length + 1,
+            nameRef.current.value,
+            priceRef.current.value
+        );
+    }
+    return (
+        <div>
+            <ul>
+                {props.items.map(i => (
+                    <Item
+                        key={i.id}
+                        name={i.name}
+                        price={i.price} />
+                ))}
+            </ul>
+            <input type="text" ref={nameRef} /><br />
+            <input type="text" ref={priceRef} /><br />
+            <button onClick={add}>Add</button>
+        </div>
+    )
 }
-class App extends React.Component {
- render() {
- return (
- <div>
- <h1>Hello React</h1>
- <ul>
- <Item name="Apple" price="0.99" />
- <Item name="Orange" price="0.89" />
- </ul>
- </div>
- )
- }
+const stateToProps = state => {
+    return {
+        items: state
+    };
 }
-export default App;
+const dispatchToProps = dispatch => {
+    return {
+        add: (id, name, price) => {
+            dispatch({
+                type: 'ADD',
+                item: { id, name, price }
+            });
+        }
+    }
+}
+
+// function add(a) {
+//     return function (b) {
+//         return a + b;
+//     }
+// }
+
+// const addTwo = add(2);
+// addTwo(3); 
+
+const ReduxApp = connect(stateToProps, dispatchToProps)(App);
+export default ReduxApp;
+
